d308c67c31e8238ed3342cd1dbd7a1a9884ec5ac
yizhou <yipersevere@outlook.com>
2017-11-08 17:26:37 +0200

    the first optimization method for reading data from mongoDB

--- fiveG/models.py
+++ fiveG/models.py
@@ -2,13 +2,14 @@ from django.db import models
 from mongoengine import *
 import numpy as np
 import pandas as pd
+from pymongo import MongoClient
 
 # Create your models here.
 
 # from mongoengine import connect
 # connect('5gopt', username='yi', password='abc123')
 
-connect(db='5gopt')
+# connect(db='5gopt')
 
 class normal(Document):
 
@@ -36,19 +37,58 @@ def preProcess():
 
 
 # for i in normal.objects[:1]:
-print("Time", normal.objects[0].Time)
-print("UeNodeNo", normal.objects[0].UeNodeNo)
-print("UeRNTI", normal.objects[0].UeRNTI)
-print("cell id", normal.objects[0].Cell_ID)
-print("RSRP", normal.objects[0].RSRP)
-print("RSRQ", normal.objects[0].RSRQ)
-print("Serving_Cell", normal.objects[0].Serving_Cell)
+# print("Time", normal.objects[0].Time)
+# print("UeNodeNo", normal.objects[0].UeNodeNo)
+# print("UeRNTI", normal.objects[0].UeRNTI)
+# print("cell id", normal.objects[0].Cell_ID)
+# print("RSRP", normal.objects[0].RSRP)
+# print("RSRQ", normal.objects[0].RSRQ)
+# print("Serving_Cell", normal.objects[0].Serving_Cell)
     # print(preProcess())
 
     # def preProcess():
     #     normalData = pd.DataFrame()
-normalData = preProcess()
-print(normalData[:5])
+# normalData = preProcess()
+# print(normalData[:5])
+
+
+
+
+
+def _connect_mongo(host, port, username, password, db):
+    """ A util for making a connection to mongo """
+
+    if username and password:
+        mongo_uri = 'mongodb://%s:%s@%s:%s/%s' % (username, password, host, port, db)
+        conn = MongoClient(mongo_uri)
+    else:
+        conn = MongoClient(host, port)
+
+
+    return conn[db]
+
+
+def read_mongo(db, collection, query={}, host='localhost', port=27017, username=None, password=None, no_id=True):
+    """ Read from Mongo and Store into DataFrame """
+
+    # Connect to MongoDB
+    db = _connect_mongo(host=host, port=port, username=username, password=password, db=db)
+
+    # Make a query to the specific DB and Collection
+    cursor = db[collection].find(query)
+
+    # Expand the cursor and construct the DataFrame
+    df =  pd.DataFrame(list(cursor))
+
+    # Delete the _id
+    if no_id:
+        del df['_id']
+
+    return df
+
+normalDF = read_mongo("5gopt", "normal", no_id=True)
+print(normalDF[:5])
+
 
 
 
